af318f0e81ec3d47a4c33dcd338652c3991691de
AlaorFurquim <alaorfurquim34@gmail.com>
2021-04-27 21:48:29 -0300

    Projeto basico de aluguel de veiculos

---
+++ Aluguel_De_Carros/Aluguel_De_Carros/AluguelDeCarro.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Aluguel_De_Carros
+{
+    class AluguelDeCarro
+    {
+        public string Nome { get; private set; }
+        public int Veiculo { get; set; }
+        public double Cpf { get; private set; }
+        public double Date { get; private set; }
+        public double Total { get;  set; }
+        public double Dia { get; set; }
+        public int Fox { get; set; }
+
+        public AluguelDeCarro(string nome, double cpf, double date)
+        {
+            Nome = nome;
+            Cpf = cpf;
+            Date = date;
+            
+        }
+        public AluguelDeCarro(string nome, int veiculo,double cpf, double date, double total, double dia) : this(nome, cpf, date)
+        {
+            Total = total;
+            Dia = dia;
+            Veiculo = veiculo;
+        }
+
+
+        
+
+        public override string ToString()
+        {
+            return "Cliente: "
+                + Nome
+                + ", "
+                + "Nascimento: "
+                + Date
+                + ", "
+                + "Cpf: "
+                + Cpf
+                + ", "
+                + "Total a pagar: "
+                + ", "
+                + Total.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
---
+++ Aluguel_De_Carros/Aluguel_De_Carros/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aluguel_De_Carros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AluguelDeCarro cliente;
+            Console.WriteLine("Digite o nome do cliente!");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a data de nascimento!");
+            double date = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o cpf!");
+            double cpf = double.Parse(Console.ReadLine());
+
+            double total;
+
+            Console.WriteLine();
+
+            cliente  = new AluguelDeCarro(nome, cpf,date);
+
+            
+            Console.WriteLine(cliente);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Digite a quantidade de dias que o cliente ira ficar com o carro!");
+            double dia = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o carro que o cliente vai pegar?");
+           
+            string veiculo = Console.ReadLine();
+
+            if (veiculo == "Fox" || veiculo == "fox"){ 
+                
+                total = 150.00 * dia;
+
+                Console.WriteLine(total);
+
+            } 
+            else if(veiculo == "Gol"|| veiculo == "gol" ){
+
+                total = 200.00 * dia;
+
+                Console.WriteLine(total);
+            }
+            else if (veiculo == "Hilux" || veiculo == "hilux")
+            {
+
+                total = 500.00 * dia;
+
+                Console.WriteLine(total);
+            }
+            else if (veiculo == "Land Rouver" || veiculo == "land rouver")
+            {
+                total = 700.00 * dia;
+
+                Console.WriteLine(total);
+            }
+            else 
+            {
+
+                Console.WriteLine("Não possuimos este veiculo!");
+            }
+        }
+
+    }
+    
+}
